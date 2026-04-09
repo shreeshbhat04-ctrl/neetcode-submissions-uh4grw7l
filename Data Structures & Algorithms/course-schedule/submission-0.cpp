b5b46a6e05e16d9cb896b5eed2597d64b2d79457
Shreesha HB <shreeshbhat04@gmail.com>
2026-04-09 17:57:45 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,32 @@
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>vec(numCourses);
+        for(auto& it:prerequisites){
+            vec[it[1]].push_back(it[0]);
+        }
+        vector<int>indegree(numCourses,0);
+        for(int i=0;i<numCourses;i++){
+            for(auto it:vec[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<indegree.size();i++){
+            if(indegree[i]==0)q.push(i);
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it:vec[node]){
+                indegree[it]--;
+                if(indegree[it]==0)q.push(it);
+            }
+
+        }if(topo.size()==numCourses)return true;
+        return false;
+    }
+};
